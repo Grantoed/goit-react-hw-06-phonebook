f0c9e05cf6c5859a36d17dcb4b0c53ce4e19bb6d
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2022-11-14 15:06:33 +0200

    hw completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,80 +1,47 @@
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
+import { useDispatch, useSelector } from 'react-redux';
+import { add, remove, changeFilter } from 'redux/slices';
+import { getContactsValue, getFilterValue } from 'redux/selectors';
 import { Section } from './Section/Section';
 import { Form } from './Form/Form';
 import { Contacts } from './Contacts/Contacts';
 import { Box } from './Box';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const filterValue = useSelector(getFilterValue);
+  const contactsArray = useSelector(getContactsValue);
 
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
-    if (parsedContacts) {
-      setContacts(parsedContacts);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const resetForm = (nameValue, numberValue) => {
-    nameValue = '';
-    numberValue = '';
-  };
-
-  const handleChange = e => {
-    switch (e.target.name) {
-      case 'contacts':
-        setContacts(e.target.value);
-        return;
-
-      case 'filter':
-        setFilter(e.target.value);
-        return;
-
-      default:
-        throw new Error('Invalid event target name');
-    }
-  };
-
-  const handleFilter = () => {
-    const normalizedFilter = filter.toLowerCase().trim();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
+  const dispatch = useDispatch();
 
   const addContact = (name, number) => {
-    const isContactAdded = contacts.some(contact => contact.name === name);
-    const newContact = { id: nanoid(3), name, number };
+    const isContactAdded = contactsArray.some(contact => contact.name === name);
 
     if (isContactAdded) {
       alert(`${name} is Already in contacts`);
     } else {
-      setContacts(prevState => [newContact, ...prevState]);
+      dispatch(add(name, number));
     }
   };
 
   const deleteContact = contactId => {
-    setContacts(prevState =>
-      prevState.filter(contact => contact.id !== contactId)
-    );
+    dispatch(remove(contactId));
   };
 
   const handleSubmit = e => {
     e.preventDefault();
     const { name, number } = e.target.elements;
     addContact(name.value, number.value);
-    resetForm(name.value, number.value);
+    e.currentTarget.reset();
+  };
+
+  const handleChange = e => {
+    dispatch(changeFilter(e.currentTarget.value));
+  };
+
+  const handleFilter = () => {
+    const normalizedFilter = filterValue.toLowerCase().trim();
+    return contactsArray.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
   };
 
   return (
@@ -87,7 +54,7 @@ export const App = () => {
           handleChange={handleChange}
           handleDelete={deleteContact}
           contacts={handleFilter()}
-          filterValue={filter}
+          filterValue={filterValue}
         ></Contacts>
       </Section>
     </Box>
